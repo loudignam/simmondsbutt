eadb53321dc0383b7c6370b8f7acd58e3ffe8ab1
ld603106 <ld603106@sju>
2016-12-05 20:04:13 -0500

    aw yeah

--- NW_Project/NW_DBConnection.php
+++ NW_Project/NW_DBConnection.php
@@ -11,10 +11,9 @@ $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-    echo 'connection success';
-} else{
-    echo'connection failed';
 }
+
+$conn->close();
 
 
 ?>
--- NW_Project/NW_Items.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include 'NW_DBConnection';
-?>
